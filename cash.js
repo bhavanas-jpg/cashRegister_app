c80fc68023f5215d26a74631e9b8bd008443da87
pavan6cs <bhavanas904@gmail.com>
2022-09-29 15:32:00 +0530

    added css styling

--- cash.js
+++ cash.js
@@ -2,8 +2,16 @@ const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
-const noOfNotes = document.querySelectorAll(".no-of-notes")
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+const enterCash = document.querySelector('#enter-cash');
+const cashContainer = document.querySelector('#cash-container');
+const showAmount = document.querySelector('#show-amount');
 const availableNotes = [2000, 500, 100,  20, 10,5, 1];
+
+enterCash.addEventListener("click", function showCashContainer(){
+    cashContainer.style.display = "flex";
+    enterCash.style.display = "none";
+})
 
 
 checkButton.addEventListener("click", function validateBillAndCashAmount(){
@@ -15,7 +23,7 @@ checkButton.addEventListener("click", function validateBillAndCashAmount(){
           const amountToBeReturned = cashGiven.value - billAmount.value;
            calculateChange(amountToBeReturned );
         }else{
-        showMessage("Do you wanna wash plates?")
+        showMessage("Do you wanna wash plates? 🤔")
         }
     }else{
        showMessage("Invalid bill amount")
@@ -23,6 +31,7 @@ checkButton.addEventListener("click", function validateBillAndCashAmount(){
 })
  
 const calculateChange = (amountToBeReturned ) =>{
+   showAmount.style.display = "block"
 for(let i=0; i < availableNotes.length; i++){
     const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]) 
         amountToBeReturned =  amountToBeReturned  % availableNotes[i];
